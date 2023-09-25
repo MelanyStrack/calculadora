a579f7da08abcf3d8c7b46c820d1758b3e0f9517
MelanyStrack <melany.strack@gmail.com>
2023-09-24 21:19:07 -0300

    Modifique el archivo calculadora y lo separe de la funcion operaciones en otro modulo

--- calculadora.js
+++ calculadora.js
@@ -1,32 +1,18 @@
-//let suma = require("./modules/suma.js");
-//let resta = require("./modules/resta.js");
-let multiplicacion = require("./modules/multiplicacion.js");
-//let division = require("./modules/division.js")
+let operaciones = require("./modules/operaciones.js")
 
 let operacion = process.argv[2]
 let numero1 = +process.argv[3]
 let numero2 = +process.argv[4]
-
-function operaciones(operacion) {
-    switch (operacion) {
-        case "suma":
-            return suma;
-        case "resta":
-            return resta ;
-        case "multiplicacion":
-            return multiplicacion;
-        case "division":
-            return division;
-        default:
-            break;
-    }
     
-}
 
 function calculadora(cb, numero1, numero2) {
+    //  const objeto = {
+    //     operacion : cb,
+    //      resultado : cb(numero1, numero2)
+    //  }
+    //  agregarOperacion(objeto)
     return cb(numero1, numero2)
 }
 
 
 console.log(calculadora(operaciones(operacion), numero1, numero2));
-//console.log(multiplicacion(numero1,numero2));
